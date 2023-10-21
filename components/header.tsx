0902e26a8c8d22f9fb4ec545ced6006ea1b73845
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-10-21 16:23:58 -0400

    UPDATED: Header component and Added Klutch Party Rentals logo
    - Updated src attribute of the logo image in the Header component from "/placeholder.png" to "/klutch-logo.png"
    - Added Klutch Party Rentals logo (klutch-logo.png) to the public folder

--- components/header.tsx
+++ components/header.tsx
@@ -10,12 +10,12 @@ export const Header: React.FC = () => {
           alt="Company Logo"
           className="object-contain object-center rounded-full"
           height={80}
-          src="/placeholder.png"
+          src="/klutch-logo.png"
           layout="fixed"
           width={80}
         />
         <h2 className="ml-2 text-2xl font-bold text-gray-900 dark:text-white sm:ml-4 sm:text-3xl">
-          event rental
+          Klutch Party Rentals
         </h2>
       </div>
       <ModeToggle />
