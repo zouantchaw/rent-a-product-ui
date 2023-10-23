ba5e4e997c7bd7514d58e11bdbd7813137a21bf1
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-10-23 12:14:54 -0400

    Update company logo in header component

--- components/header.tsx
+++ components/header.tsx
@@ -27,11 +27,11 @@ export const Header: React.FC<HeaderProps> = ({ locale, setLocale }) => {
           alt="Company Logo"
           className="object-contain object-center rounded-full"
           height={80} 
-          src="/klutch-logo.png"
+          src="/diane-logo.png"
           layout="fixed"
           width={80}
         />
-        <h2 className="ml-2 text-2xl font-bold text-gray-900 dark:text-white sm:ml-4 sm:text-3xl">
+        <h2 className="ml-2 text-2xl sm:text-1xl font-bold text-gray-900 dark:text-white sm:ml-4 sm:text-3xl">
           Diane Party Rentals
         </h2>
       </div>
