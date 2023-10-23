c1b5b188dd777f342581896f2d74ae283a1fe7c6
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-10-23 06:31:23 -0400

    Add quantity input and add to cart functionality

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge"
-
+import { Badge } from "@/components/ui/badge";
+import { CartItem } from "@/components/landing";
 
 interface ProductCardProps {
   alt: string;
@@ -11,9 +11,33 @@ interface ProductCardProps {
   title: string;
   price: number;
   color: string;
+  cart: CartItem[];
+  setCart: React.Dispatch<React.SetStateAction<CartItem[]>>;
 }
 
-export const ProductCard: React.FC<ProductCardProps> = ({ alt, src, title, price, color }) => {
+export const ProductCard: React.FC<ProductCardProps> = ({
+  alt,
+  src,
+  title,
+  price,
+  color,
+  cart,
+  setCart,
+}) => {
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const addToCart = () => {
+    const newCartItem: CartItem = { id: title, quantity: quantity };
+    const existingCartItemIndex = cart.findIndex((item) => item.id === title);
+    if (existingCartItemIndex > -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingCartItemIndex].quantity += quantity;
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, newCartItem]);
+    }
+  };
+
   return (
     <div className="flex flex-col items-start bg-white dark:bg-gray-700 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-200">
       <div className="w-full h-64 relative">
@@ -29,20 +53,18 @@ export const ProductCard: React.FC<ProductCardProps> = ({ alt, src, title, price
         {title}
       </h3>
       <div className="flex flex-row space-x-2">
-        <Badge className="text-md font-medium">
-          {color}
-        </Badge>
-        <Badge className="text-md font-medium">
-          ${price}
-        </Badge>
+        <Badge className="text-md font-medium">{color}</Badge>
+        <Badge className="text-md font-medium">${price}</Badge>
       </div>
       <input
         className="mt-2 p-2 w-full border border-gray-300 bg-white dark:bg-gray-800 rounded-md"
         min="1"
         placeholder="Quantity"
         type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
       />
-      <Button className="mt-2 w-full" variant="outline">
+      <Button className="mt-2 w-full" variant="outline" onClick={addToCart}>
         Add to Cart
       </Button>
     </div>
