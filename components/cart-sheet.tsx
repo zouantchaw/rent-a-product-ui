96e8228cc76b056443d1f10638214da7e26839ea
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-10-23 10:42:08 -0400

    Refactor cart-tabs component to use CartStepper

--- components/cart-sheet.tsx
+++ components/cart-sheet.tsx
@@ -10,7 +10,7 @@ import {
 import { CartButton } from "@/components/ui/cart-button";
 import { Button } from "@/components/ui/button";
 import { CartItem } from "@/components/landing";
-import { CartTabs } from "./cart-tabs";
+import { CartStepper } from "./cart-tabs";
 
 interface CartSheetProps {
   sheetSide: "top" | "right" | "bottom" | "left" | null | undefined;
@@ -69,7 +69,7 @@ export const CartSheet: React.FC<CartSheetProps> = ({
             <Button variant="default">Checkout</Button>
           </CardFooter>
         </Card> */}
-        <CartTabs />
+        <CartStepper />
       </SheetContent>
     </Sheet>
   );
--- components/cart-tabs.tsx
+++ components/cart-tabs.tsx
@@ -1,26 +1,26 @@
-import { TabsTrigger, TabsList, TabsContent, Tabs } from "@/components/ui/tabs"
-import { CardTitle, CardHeader, CardContent, Card, CardFooter } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
+import React, { useState } from "react";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
-export function CartTabs() {
+export function CartStepper() {
+  const [step, setStep] = useState(0);
+
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
   return (
-    <Tabs className="w-full max-w-3xl" defaultValue="cart">
-      <TabsList className="grid w-full grid-cols-4">
-        <TabsTrigger value="cart">Cart</TabsTrigger>
-        <TabsTrigger value="event">Event</TabsTrigger>
-        <TabsTrigger value="confirm">Sign</TabsTrigger>
-        <TabsTrigger value="pay">Pay</TabsTrigger>
-      </TabsList>
-      <TabsContent value="cart">
-        <Card>
+    <div className="w-full max-w-3xl transition-all duration-500 ease-in-out">
+      {step === 0 && (
+        <Card className="transition-all duration-500 ease-in-out">
           <CardHeader>
             <CardTitle>Your Cart</CardTitle>
           </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="border p-4 rounded-md">
+          <CardContent className="space-y-4 transition-all duration-500 ease-in-out">
+            <div className="border p-4 rounded-md transition-all duration-500 ease-in-out">
               <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-2">
                   <img
@@ -40,13 +40,23 @@ export function CartTabs() {
               <div className="flex items-center justify-between mt-2">
                 <div className="text-sm text-gray-500">Quantity:</div>
                 <div className="flex items-center space-x-2">
-                  <Button variant="outline">-</Button>
+                  <Button
+                    className="transition-all duration-500 ease-in-out"
+                    variant="outline"
+                  >
+                    -
+                  </Button>
                   <div>1</div>
-                  <Button variant="outline">+</Button>
+                  <Button
+                    className="transition-all duration-500 ease-in-out"
+                    variant="outline"
+                  >
+                    +
+                  </Button>
                 </div>
               </div>
             </div>
-            <div className="border p-4 rounded-md">
+            <div className="border p-4 rounded-md transition-all duration-500 ease-in-out">
               <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-2">
                   <img
@@ -66,42 +76,35 @@ export function CartTabs() {
               <div className="flex items-center justify-between mt-2">
                 <div className="text-sm text-gray-500">Quantity:</div>
                 <div className="flex items-center space-x-2">
-                  <Button variant="outline">-</Button>
+                  <Button
+                    className="transition-all duration-500 ease-in-out"
+                    variant="outline"
+                  >
+                    -
+                  </Button>
                   <div>1</div>
-                  <Button variant="outline">+</Button>
+                  <Button
+                    className="transition-all duration-500 ease-in-out"
+                    variant="outline"
+                  >
+                    +
+                  </Button>
                 </div>
               </div>
             </div>
-            {/* <Button className="w-full mt-4">Proceed to Confirmation</Button> */}
-            <Button className="w-full flex justify-between hover:animate-pulse" variant="default">
-              <span>Proceed to Checkout</span>
-              <span className="flex items-center">
-                Total: $170
-                <svg
-                  className=" ml-2 animate-slide"
-                  fill="none"
-                  height="24"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M5 12h14" />
-                  <path d="m12 5 7 7-7 7" />
-                </svg>
-              </span>
+            <Button
+              onClick={nextStep}
+              className="w-full mt-4 transition-all duration-500 ease-in-out"
+            >
+              Proceed to Confirmation
             </Button>
           </CardContent>
         </Card>
-      </TabsContent>
-      <TabsContent className="transition-all duration-500 ease-in-out" value="cart" />
-      <TabsContent className="transition-all duration-500 ease-in-out" value="event">
+      )}
+      {step === 1 && (
         <Card className="transition-all duration-500 ease-in-out">
           <CardHeader>
-            <CardTitle>Event Details</CardTitle>
+            <CardTitle>Confirm & Sign</CardTitle>
           </CardHeader>
           <CardContent className="space-y-4 transition-all duration-500 ease-in-out">
             <div className="space-y-2 transition-all duration-500 ease-in-out">
@@ -116,52 +119,64 @@ export function CartTabs() {
               <Label htmlFor="eventLocation">Event Location</Label>
               <Input id="eventLocation" placeholder="Enter event location" />
             </div>
-            <Button className="w-full mt-4 transition-all duration-500 ease-in-out">Proceed to Confirmation</Button>
+            <Button
+              onClick={nextStep}
+              className="w-full mt-4 transition-all duration-500 ease-in-out"
+            >
+              Proceed to Payment
+            </Button>
           </CardContent>
         </Card>
-      </TabsContent>
-      <TabsContent value="confirm">
-        <Card>
+      )}
+      {step === 2 && (
+        <Card className="transition-all duration-500 ease-in-out">
           <CardHeader>
             <CardTitle>Confirm & Sign</CardTitle>
           </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="flex items-start">
+          <CardContent className="space-y-4 transition-all duration-500 ease-in-out">
+            <div className="flex items-start transition-all duration-500 ease-in-out">
               <Checkbox id="terms" />
               <Label className="ml-2" htmlFor="terms">
                 I accept the terms and conditions
               </Label>
             </div>
-            <div className="space-y-2">
+            <div className="space-y-2 transition-all duration-500 ease-in-out">
               <Label htmlFor="signature">Signature</Label>
               <Input id="signature" placeholder="Sign here" />
             </div>
-            <Button className="w-full mt-4">Proceed to Payment</Button>
+            <Button
+              onClick={nextStep}
+              className="w-full mt-4 transition-all duration-500 ease-in-out"
+            >
+              Proceed to Payment
+            </Button>
           </CardContent>
         </Card>
-      </TabsContent>
-      <TabsContent value="pay">
-        <Card>
+      )}
+      {step === 3 && (
+        <Card className="transition-all duration-500 ease-in-out">
           <CardHeader>
             <CardTitle>Pay</CardTitle>
           </CardHeader>
-          <CardContent>
-            <div className="space-y-2">
+          <CardContent className="space-y-2 transition-all duration-500 ease-in-out">
+            <div className="space-y-2 transition-all duration-500 ease-in-out">
               <Label htmlFor="cardNumber">Card Number</Label>
               <Input id="cardNumber" placeholder="Enter your card number" />
             </div>
-            <div className="space-y-2">
+            <div className="space-y-2 transition-all duration-500 ease-in-out">
               <Label htmlFor="expiryDate">Expiry Date</Label>
               <Input id="expiryDate" placeholder="MM/YY" />
             </div>
-            <div className="space-y-2">
+            <div className="space-y-2 transition-all duration-500 ease-in-out">
               <Label htmlFor="cvc">CVC</Label>
               <Input id="cvc" placeholder="CVC" />
             </div>
-            <Button className="w-full mt-4">Complete Payment</Button>
+            <Button className="w-full mt-4 transition-all duration-500 ease-in-out">
+              Complete Payment
+            </Button>
           </CardContent>
         </Card>
-      </TabsContent>
-    </Tabs>
-  )
+      )}
+    </div>
+  );
 }
